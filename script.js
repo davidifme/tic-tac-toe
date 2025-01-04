db3bf321e7c232350a8a85564c1104adafc9c9aa
David <davidifme@icloud.com>
2025-01-04 10:04:47 +0100

    create 3x3 gameboard

---
+++ script.js
@@ -0,0 +1,21 @@
+function Gameboard() {
+    let rows = 3;
+    let columns = 3;
+    let gameboard = [];
+
+    for (let i = 0; i < rows; i++) {
+        let row = [];
+        for (let j = 0; j < columns; j++) {
+            row.push('')
+        }
+        gameboard.push(row);
+    }
+}
+
+function Player() {
+
+}
+
+function GameFlow() {
+    
+}
