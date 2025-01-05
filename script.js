a1ab96a7d917bfb0293b960104c37e6998a39896
David <davidifme@icloud.com>
2025-01-05 16:49:16 +0100

    add console gaming support

--- script.js
+++ script.js
@@ -1,21 +1,121 @@
 function Gameboard() {
-    let rows = 3;
-    let columns = 3;
-    let gameboard = [];
+    const rows = 3;
+    const columns = 3;
+    let board = [];
 
     for (let i = 0; i < rows; i++) {
-        let row = [];
+        board[i] = [];
         for (let j = 0; j < columns; j++) {
-            row.push('')
+            board[i].push(Cell())
         }
-        gameboard.push(row);
     }
+
+    const getBoard = () => board;
+
+    const printBoard = () => {
+        const boardWithCellValues = board.map((row) => row.map((cell) => cell.getCellValue()));
+        console.log(boardWithCellValues);
+    }
+
+    const clearBoard = () => {
+        board.map(row => row.map(cell => cell.setCellValue('')));
+    };
+
+    return { getBoard, printBoard, clearBoard }
 }
 
-function Player() {
+function Cell() {
+    let value = '';
 
+    const setCellValue = (tokenType) => {
+        value = tokenType;
+    };
+    const getCellValue = () => value;
+
+    return { setCellValue, getCellValue }
 }
 
-function GameFlow() {
-    
+function GameFlow(playerOneName = 'Player One', playerTwoName = 'Player Two') {
+    const board = Gameboard();
+
+    const players = [
+        {
+            name: playerOneName,
+            token: 'X'
+        },
+        {
+            name: playerTwoName,
+            token: 'O'
+        }
+    ];
+
+    let activePlayer = players[0];
+
+    const switchPlayerTurn = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+      };
+    const getActivePlayer = () => activePlayer;
+
+    const printNewRound = () => {
+        board.printBoard();
+        console.log(`${getActivePlayer().name}'s turn.`)
+    }
+
+    const resetGame = () => {
+        board.clearBoard();
+        activePlayer = players[0];
+    };
+
+    const checkWinner = () => {
+        const b = board.getBoard().map(row => row.map(cell => cell.getCellValue()));
+        const currentToken = activePlayer.token;
+
+        // Check rows
+        for (let i = 0; i < 3; i++) {
+            if (b[i][0] === currentToken && b[i][1] === currentToken && b[i][2] === currentToken) {
+                return true;
+            }
+        }
+
+        // Check columns
+        for (let i = 0; i < 3; i++) {
+            if (b[0][i] === currentToken && b[1][i] === currentToken && b[2][i] === currentToken) {
+                return true;
+            }
+        }
+
+        // Check diagonals
+        if (b[0][0] === currentToken && b[1][1] === currentToken && b[2][2] === currentToken) {
+            return true;
+        }
+        if (b[0][2] === currentToken && b[1][1] === currentToken && b[2][0] === currentToken) {
+            return true;
+        }
+
+        return false;
+    };
+
+    const playRound = (row, column) => {
+        if (board.getBoard()[row][column].getCellValue() === '') {
+            board.getBoard()[row][column].setCellValue(activePlayer.token);
+
+            if (checkWinner()) {
+                board.printBoard();
+                console.log(`${getActivePlayer().name} wins!`)
+                resetGame();
+                return;
+            }
+
+            switchPlayerTurn();
+            printNewRound()
+        } else {
+            return console.log(`This cell is occupied!`)
+        }
+    };
+
+    printNewRound();
+
+    return { playRound, getActivePlayer, resetGame }
 }
+
+const game = GameFlow();
